17614d6732a54c55156656dc0bc850569565b513
Ahmad Hazrati <hazrati.ahmad@gmail.com>
2023-09-22 12:48:39 +0000

    Update heroku app hostname in the allowed hostname of the settings.py

--- thinkgreen/settings.py
+++ thinkgreen/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['think-green-blog.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['think-green-blog-91022c17e136.herokuapp.com', 'localhost']
 
 
 # Application definition
